0e57f20d1c0488c6f22be4dc9da1297cfe1634a9
Nathan Hennig <Nathan.Hennig@gmail.com>
2020-07-18 13:56:23 -0700

    Fixes REXML mutliple root issue by wrapping response body in a top level   tag, so API responses including multiple transactions will work. Large scale coding sylte improvements.

--- lib/velocity/velocity_connection.rb
+++ lib/velocity/velocity_connection.rb
@@ -1,5 +1,5 @@
 require "base64"
-require 'httparty'
+require "httparty"
 require_relative "velocity_exception"
 require_relative "velocity_processor"
 require_relative "velocity_xml_creator"
@@ -9,7 +9,7 @@ include Velocity::VelocityException::VelocityErrorMessages
 module Velocity
   class VelocityConnection
     attr_accessor :session_token, :identity_token
-    
+
     def initialize(identity_token)
       @identity_token = identity_token
     end
@@ -17,38 +17,39 @@ module Velocity
     def identity_token
       @identity_token
     end
-    
-    SIGNON_URL = if Rails.env == "production"
+
+    SIGNON_URL = if Rails.env.production?
                    "https://api.nabcommerce.com/REST/2.0.18/SvcInfo/token"
                  else
                    "https://api.cert.nabcommerce.com/REST/2.0.18/SvcInfo/token"
                  end
-   # ----------------------------> signOn Method  <----------------------------- # 
-    
-   # "signOn" method for making GET request.
-   # In this method to genrate the session token by passing the identity_token.
-   # It returns the session token in string.
+    # ----------------------------> signOn Method  <----------------------------- #
 
-    def signOn()
-      begin
-        response_xml = HTTParty.get(SIGNON_URL,
-        headers: { "Content-Type" => "application/json", 
-          "Authorization" => "Basic #{encode_token(identity_token)}"},
-        verify: false,:timeout => 60 )
-        encode_token(response_xml)
-      rescue Exception => ex
-        return "invalid session token"
-      end
+    # "signOn" method for making GET request.
+    # In this method to genrate the session token by passing the identity_token.
+    # It returns the session token in string.
+
+    def signOn
+      response_xml = HTTParty.get(
+        SIGNON_URL,
+        headers: {
+          "Content-Type"  => "application/json",
+          "Authorization" => "Basic #{encode_token(identity_token)}"
+        },
+        verify:  false,
+        timeout: 60
+      )
+      encode_token(response_xml)
+    rescue StandardError => e
+      "Invalid session token - #{e.message}"
     end
-    
+
     private
-    
+
     def encode_token(str)
-      begin
-        Base64.strict_encode64(str.gsub(/"/, '').concat(":"))
-      rescue Exception => ex
-        return "invalid identity token"
-      end
+      Base64.strict_encode64(str.gsub(/"/, '').concat(":"))
+    rescue StandardError => e
+      "Invalid identity token - #{e.message}"
     end
   end
 end
